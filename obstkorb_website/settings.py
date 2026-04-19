b9862311a172a6dec69cfe938d4fd66d80eb749c
F.Ege Eryigit <f.ege.eryigit@gmail.com>
2026-04-19 20:04:59 +0300

    setting.py email fix

--- obstkorb_website/settings.py
+++ obstkorb_website/settings.py
@@ -145,9 +145,5 @@ SESSION_COOKIE_AGE = 60 * 60 * 24 * 2
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 # Email
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp-relay.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-DEFAULT_FROM_EMAIL = "Sarısoy Obstkorb <info@sarisoyobstkorb.de>"
+RESEND_API_KEY = os.getenv("RESEND_API_KEY")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
