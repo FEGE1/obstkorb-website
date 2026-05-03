ab80bac83c57e0a0b10001773823c1280e1b791d
F.Ege Eryigit <f.ege.eryigit@gmail.com>
2026-05-03 20:02:54 +0300

    admin.py update

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from product.models import Product, Order, OrderItem
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
